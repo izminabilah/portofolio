b970f0f6c817bec7ad01d76d0784d1589b9a913e
izminabilah <izminabilah13@gmail.com>
2026-02-21 15:23:49 +0700

    membuat navbar

--- portofolio/src/App.jsx
+++ portofolio/src/App.jsx
@@ -2,7 +2,7 @@
 function App() {
   return (
     <>
-      <h1>jelllo</h1>
+      <h1 className="text-3xl">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti illo libero similique natus asperiores dolor, assumenda porro, suscipit dolores voluptatibus et veritatis ad aliquam possimus necessitatibus dicta accusamus hic sint!</h1>
     </>
   )
 }
